794da07c30403f558d454763c39fac4917c1cc5b
AleGuilherme <alliux1977@gmail.com>
2021-01-14 14:50:21 -0300

    [Add] Sencond commit

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
             </header>
             <article>
                <p>TESTES DEPLOY CORPO.</p>
+               <img src="https://www.google.com.br/imgres?imgurl=https%3A%2F%2Fs2.glbimg.com%2F4L-EwnA1GBSJQjuRWLKviwljfR4%3D%2F850x446%2Fs.glbimg.com%2Fpo%2Ftt%2Ff%2F620x388%2F2011%2F12%2F05%2Ftux_linux_wallpapers_free_desktop_with_the_penguin_wallpaper-1024x768.jpg&imgrefurl=http%3A%2F%2Fwww.techtudo.com.br%2Fartigos%2Fnoticia%2F2011%2F12%2Fo-que-e-linux.html&tbnid=xc8kO6ulLiJlcM&vet=12ahUKEwivkauT_pvuAhV4JrkGHd6oA4QQMygDegUIARCrAQ..i&docid=yPHv5C34RQxO4M&w=850&h=446&q=linux&ved=2ahUKEwivkauT_pvuAhV4JrkGHd6oA4QQMygDegUIARCrAQ">
              </article>
             <footer>
               <p>Alexandre Guilherme</p>
