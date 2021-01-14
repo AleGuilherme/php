6130af8178b73113f7bd92834e8fea8504367d4e
AleGuilherme <alliux1977@gmail.com>
2021-01-14 14:43:36 -0300

    [Add] Sencond commit

--- index.php
+++ index.php
@@ -1,1 +1,32 @@
-# Aula deploy
+<html>
+     <head>
+        <title>TESTES COM DEPLOY</title>
+     </head>
+     <body>
+        <header>
+            <h1>ALEXANDRE GUILHERME</h1>
+        </header>
+        <nav>
+            <ul>
+              <li><a href="#">Inicial</a></li>
+              <li><a href="#">Quem somos</a></li>
+              <li><a href="#">Contato</a></li>
+            </ul>
+        </nav>
+        <section id="example">
+            <header>
+              <h2>TESTES DEPLOY</h2>
+            </header>
+            <article>
+               <p>TESTES DEPLOY CORPO.</p>
+             </article>
+            <footer>
+              <p>Alexandre Guilherme</p>
+            </footer>
+        </section>
+        <aside> </aside>
+        <footer>
+            <p>Todos os direitos reservados.</p>
+        </footer>
+     </body>
+  </html>
